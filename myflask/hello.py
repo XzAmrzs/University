04f2f1169dcddf26ee36dd1343b27f7ffad5a2b5
XzAmrzs <pcxzp@live.com>
2015-07-07 21:38:54 +0000

    添加渲染模板文件

--- myflask/hello.py
+++ myflask/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.script import Manager
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -7,11 +8,11 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!<h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!<h1>' %name
+    return render_template('user.html',name = name)
 
 from flask import redirect
 @app.route('/user')
