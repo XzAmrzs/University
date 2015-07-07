6b0fa09ba034af1562c907d90fbddc77e431fdfd
XzAmrzs <pcxzp@live.com>
2015-07-07 16:21:05 +0000

    添加页面重定向函数redirect()

--- myflask/hello.py
+++ myflask/hello.py
@@ -7,7 +7,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello %s!<h1>' %name
-
+from flask import redirect
+@app.route('/user')
+def redir():
+    return redirect('http://www.baidu.com')
 if __name__ == '__main__':
    app.run(debug=True)
 
