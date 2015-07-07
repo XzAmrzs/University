570c55a5d46a26bd2166ae34d413301f599b1d40
XzAmrzs <pcxzp@live.com>
2015-07-07 18:20:33 +0000

    安装Flask-Script拓展

--- myflask/hello.py
+++ myflask/hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask.ext.script import Manager
+
 app=Flask(__name__)
+
+manager = Manager(app)
+
 @app.route('/')
 def index():
     return '<h1>Hello World!<h1>'
@@ -7,10 +12,12 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello %s!<h1>' %name
+
 from flask import redirect
 @app.route('/user')
 def redir():
     return redirect('http://www.baidu.com')
+
 if __name__ == '__main__':
-   app.run(debug=True)
+   manager.run()
 
