2a789840cc8e000f0e1d117454909a280d5fe298
XzAmrzs <pcxzp@live.com>
2015-07-08 17:12:08 +0000

    添加Flask-Moment模块，设置时间戳

--- myflask/hello.py
+++ myflask/hello.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask.ext.script import Manager
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app=Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -18,7 +21,7 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
