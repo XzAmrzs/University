1ec3488f9aa6b6a40710a73c8e7ff695319ddd80
XzAmrzs <pcxzp@live.com>
2015-07-15 15:09:06 +0000

    添加web表单存储功能

--- myflask/hello.py
+++ myflask/hello.py
@@ -52,14 +52,16 @@ def redir():
     return redirect('http://www.baidu.com')
 
 
+from flask import Flask, render_template, session, redirect, url_for
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
