196f447b629796caaef805ac5596da8c9e5bd4b6
XzAmrzs <pcxzp@live.com>
2015-07-15 14:50:39 +0000

    添加web表单

--- myflask/hello.py
+++ myflask/hello.py
@@ -1,38 +1,70 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
-from flask.ext.script import Manager
 from flask import render_template
+from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 from datetime import datetime
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(Form):
+    name = StringField('What is your name ?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name = name)
+    return render_template('user.html', name=name)
+
 
 from flask import redirect
+
+
 @app.route('/user')
 def redir():
     return redirect('http://www.baidu.com')
 
 
-if __name__ == '__main__':
-   manager.run()
 
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+
+if __name__ == '__main__':
+    manager.run()
